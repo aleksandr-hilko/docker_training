0576b9768b7805d7150ad26f5d7b35bceb24881b
alexander.khilko <alexkhilko92@gmail.com>
2019-09-19 15:12:54 +0300

    fix tests

--- web/tests.py
+++ web/tests.py
@@ -5,12 +5,6 @@ class MainTestCase(unittest.TestCase):
     def test_two_and_two(self):
         four = 2 + 2
         self.assertEqual(four, 4)
-        self.assertEqual(four, 5)
-        self.assertEqual(2, 2)
-        self.assertNotEqual(four, 5)
-        self.assertNotEqual(four, 6)
-        self.assertNotEqual(four, 22)
-
 
 if __name__ == "__main__":
     unittest.main()
